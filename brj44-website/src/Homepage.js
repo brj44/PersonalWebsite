99c75535d2e4e1b8803f440b09ed074965d971fc
Benito Juarez <brj44@txstate.edu>
2023-03-10 13:58:40 -0600

    Updated resume and manifest

--- brj44-website/src/Homepage.js
+++ brj44-website/src/Homepage.js
@@ -12,6 +12,14 @@ return(
                     with a minor in Applied Mathematics. I am also a member of the Efficient Computing Laboratory (ECL) under
                     Dr. Martin Burtscher. My area of research is lossless audio compression where I hope to develop a more efficient compression algorithm.
                 </p>
+                <br/>
+                <p>
+                    I am also currently a front-end developer at SpexoDisks led by Dr. Banzatti. I am utilizing my previous knowledge of web development
+                    that I have acquired from my previous experiences to create a better user experience on the website. As we are a small team, I take on
+                    multiple roles under my title of front-end developer. I am responsible for creating the UI/UX of the website ensuring that our users can tell
+                    the story of our website in a natural and intuitive way. I am also responsible for creating the front-end of the website using React and my main
+                    focus is refactoring the code to make it more easier to maintain and more dynamic to different screen sizes along with adding creating new components.
+                </p>
                 <h2>Contact:</h2>
                 <p>
                     LinkedIn: <a href="https://www.linkedin.com/in/benito-juarez-92815a221/">https://www.linkedin.com/in/benito-juarez-92815a221/</a>
